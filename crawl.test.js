382e248bc54b7851ff9ac731b346002dbfb7b1f2
berlin <aeourmassi@gmail.com>
2023-09-20 13:09:53 +0100

    normlize url and tests

---
+++ crawl.test.js
@@ -0,0 +1,28 @@
+const { normalizeURL } = require("./crawl.js");
+const { test, expect } = require("@jest/globals");
+test("normalizeURL strip protocol", () => {
+  const input = "https://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip trailling slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip capitals", () => {
+  const input = "https://BLOG.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+test("normalizeURL strip http", () => {
+  const input = "http://BLOG.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
